7e2fce0332067035d005de342bcc02e2b1ef7a5d
jonnycyr <jjcyr527@gmail.com>
2018-07-08 19:00:15 -0400

    Added in tagging for function

--- code/jonny-org.el
+++ code/jonny-org.el
@@ -1,18 +1,22 @@
 ;;; jonny-org.el --- elisp functions that do org-related things
 
+
 ;;; Commentary:
 
 
 ;;; Code:
+(require 'org)
+
 (defun jonny-create-project()
   "Asks for the project name, description, and url - then inserts an org entry based on that."
   (interactive)
   (let ((project-name (read-from-minibuffer "Enter the project name: "))
 	(project-desc (read-from-minibuffer "Enter a small description: "))
-	(project-url (read-from-minibuffer "Enter a project url: ")))
+	(project-url (read-from-minibuffer "Enter a project url: "))
+	(project-tag (read-from-minibuffer "Enter a tag: ")))
   (insert (format "* %s
 %s\n
-%s
+%s\n
 ** tasks
 ** testing
 *** setting up test environment
@@ -26,7 +30,10 @@
 		  project-name
 		  project-desc
 		  project-url
-		  ))))
+		  ))
+  (search-backward (format "* %s" project-name))
+  (org-set-tags-to project-tag)
+  ))
 
 (provide 'jonny-org)
 ;;; jonny-org.el ends here
